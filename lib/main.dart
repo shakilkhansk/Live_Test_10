1f740806cc3e3c8e53b833dc2b3cac2566f75786
SHAKIL <shakilkhanking@gmail.com>
2023-09-29 23:35:05 +0600

    live project

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,87 @@
 import 'package:flutter/material.dart';
 
-main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity(),);
+    return MaterialApp(
+      home: ImageFeed(),
+    );
   }
 }
-class HomeActivity extends StatefulWidget{
-  const HomeActivity({super.key});
 
+class ImageFeed extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _HomeActivityState();
-
+  _ImageFeedState createState() => _ImageFeedState();
 }
-class _HomeActivityState extends State<HomeActivity>{
-  var products = [
-    {'name':'Product 01','price': '50'},
-    {'name':'Product 02','price': '10'},
-    {'name':'Product 03','price': '15'},
-    {'name':'Product 04','price': '20'},
-    {'name':'Product 05','price': '25'},
-    {'name':'Product 06','price': '30'},
-    {'name':'Product 07','price': '35'},
-    {'name':'Product 08','price': '40'},
-    {'name':'Product 09','price': '50'},
-    {'name':'Product 10','price': '75'},
+
+class _ImageFeedState extends State<ImageFeed> {
+  List<String> images = [
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    "https://via.placeholder.com/150",
+    // Add more image URLs here
   ];
-  List<int> productCounters = List.generate(10, (index) => 0);
-  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product List'),
-        centerTitle: true,
+        title: Text("Image Feed"),
       ),
-      body: ListView.builder(
-          shrinkWrap: true,
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(products[index]['name']!),
-              subtitle: Text('Price: '+products[index]['price']!),
-              trailing: Column(
-                  children: [
-                    Text("Counter: ${productCounters[index]}"),
-                    Expanded(child: ElevatedButton(onPressed: (){
-                      if(productCounters[index]<=4){
-                        setState(() {
-                          productCounters[index]++;
-                          if(productCounters[index]==5){
-                            MyAlertDialoge(context,products[index]['name']! );
-                          }
-                          counter = productCounters.where((element) => element != 0).length;
-                        });
-                      }
-                    }, child: Text('Buy Now')))
-                  ],
-                ),
-            );
-          },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CartPage(counter.toString())));
+      body: isPortrait
+          ? ListView.builder(
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return ImageCard(imageUrl: images[index]);
         },
-        child: Icon(Icons.shopping_cart_sharp),
+      )
+          : GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return ImageCard(imageUrl: images[index]);
+        },
       ),
     );
   }
 }
 
-class CartPage extends StatefulWidget {
-  String total = '0';
-  CartPage(this.total, {super.key});
+class ImageCard extends StatelessWidget {
+  final String imageUrl;
 
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
+  ImageCard({required this.imageUrl});
 
-class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Cart"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Total Products: ${widget.total}"),
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.network(imageUrl, width: 150, height: 150, fit: BoxFit.cover),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            // child: Text("Image Description"),
+          ),
+        ],
       ),
     );
   }
-}
-
-
-MyAlertDialoge(context,String product){
-  return showDialog(context: context, builder: (BuildContext context) {
-    return Expanded(child: AlertDialog(
-      title: Text('Congratulations!'),
-      content: Text("You've bought 5 $product"),
-      actions: [
-        TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text('OK')),
-      ],
-    ));
-  });
 }
